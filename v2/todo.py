eca61352bc988aeafd269ba67c017ff533d9b57b
Oakland Peters <oakland.peters@gmail.com>
2016-01-26 11:27:21 -0500

    Revise todo notes.

--- v2/todo.py
+++ v2/todo.py
@@ -1,11 +1,15 @@
 """
 Next-steps:
 * IMPORTANT REWORK: Morphism IS a Monoid, but defined in terms of different functions on the category: zero --> identity, append --> compose, join --> collapse
-* Rework so that functions that can be defined in terms of one another ARE -- inside category.Element, and category.Morphism (such as bind in terms of fmap and join). Particularly, f_map, a_map, and m_map can be defined on category.Morphism.
-* new class: 'WellBehavedMonad', which provides implementation of a number of conveniences, including the dispatching __new__, __repr__ based on .data, .iter, 
-* IMPORTANT: decide what/how to do with 'compose' for morphisms. Possible: zero==identity, append==compose, join==collapse
-* Add '@functools.wraps()' statements to 'map' functions in list.py and maybe.py. Consider changing the naming process.
-* Simplify List.compose - to account for removing the identity function when appending ot it
+** Apply to list.py
+* Add __instancecheck__ and __subclasscheck__ to category.Category, and make ListCategory used as a metaclass. (lookup how to do this from mfpy). Hard part - do it while allowing normal 'isinstance' to work when not overridden. 
+* Just/Nothing should override __instancecheck__ and __subclasscheck__
+* Rework so that functions that can be defined in terms of one another ARE
+** category.Category.f_map <-- f_apply
+** category.Category.a_map <-- a_apply
+** category.Category.m_map <-- m_apply
+** m_apply in terms of f_apply and join
+* Simplify List.compose/Maybe.compose - to account for removing the identity function when appending ot it
 
 
 Later steps:
@@ -14,6 +18,8 @@ Later steps:
 * Refactor methods on Element/Morphism to refer to Functor/Applicative/Monad. Do this by placing references in the category: List.functor = ListFunctor. Requires some notion of how I want functor/applicative to be used.
 * Make category.Category abstract, and define what functions it can, in terms of one another.
 * Rework category.py to be abstract classes (Monad, etc).
+* new class: 'WellBehavedMonad', which provides implementation of a number of conveniences, including the dispatching __new__, __repr__ based on .data, .iter, 
+* Add '@functools.wraps()' statements to 'map' functions in list.py and maybe.py. Consider changing the naming process.
 
 
 Much-later steps:
