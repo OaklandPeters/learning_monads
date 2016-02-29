462160ba2e54a9a6e24de249c2bcab807ebeef1f
Oakland Peters <oakland.peters@gmail.com>
2016-02-29 13:18:42 -0500

    element uses basis functions.

--- v4/foundations/category/basis.py
+++ v4/foundations/category/basis.py
@@ -1,8 +1,33 @@
-from ...support.typecheckable import TypeCheckableMeta
+from ...support.typecheckable import TypeCheckableMeta, meets
 from ...support.methods import abstractpedanticmethod
 
 
 class Applicative(metaclass=TypeCheckableMeta):
     @abstractpedanticmethod
     def apply(cls, element: 'Element', morphism: 'Morphism') -> 'Element':
+        return NotImplemented
+
+    @classmethod
+    def __subclasshook__(cls, C):
+        if cls is Applicative:
+            return meets(C, cls.__abstractmethods__)
+        return NotImplemented
+
+
+class Equitable(metaclass=TypeCheckableMeta):
+    """
+    Almost all classes will inherit __eq__ from 'object', but many should
+    actually override this anyway.
+    """
+    @abstractpedanticmethod
+    def __eq__(self, other):
+        return NotImplemented
+
+    def __ne__(self, other):
+        return not self == other
+
+    @classmethod
+    def __subclasshook__(cls, C):
+        if cls is Equitable:
+            return meets(C, cls.__abstractmethods__)
         return NotImplemented
--- v4/foundations/category/element.py
+++ v4/foundations/category/element.py
@@ -4,13 +4,11 @@ Difference in terms:
 In category-theory, what is called the *objects* in a category, we are calling the *elements* of a category - to prevent name-collusions (and confusions) with Pythons builtin 'object' class.
 Note - Python's 'object' class, actually forms the 'elements' for the category of all objects representable in Python (which I'm calling Pysk).
 """
-from ...support.methods import abstractpedanticmethod
 from ...support.typecheckable import TypeCheckableMeta, TypeCheckable, meets
+from .basis import Applicative, Equitable
 
-from .basis import Applicative
 
-
-class Element(Applicative, TypeCheckable, metaclass=TypeCheckableMeta):
+class Element(Applicative, Equitable, TypeCheckable, metaclass=TypeCheckableMeta):
     """
     Abstract methods:
         apply
