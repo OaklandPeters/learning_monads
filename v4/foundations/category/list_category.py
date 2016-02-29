dbcbf088e1999a023dc127c1a903ef59bcdbf103
Oakland Peters <oakland.peters@gmail.com>
2016-02-29 13:19:56 -0500

    Cleanup list_category

--- v4/foundations/category/list_category.py
+++ v4/foundations/category/list_category.py
@@ -3,6 +3,19 @@ Example category. Doubles as unit-test of Category, Element, Morphism
 
 One version made with StandardCategory, and one done the verbose way,
 so unit-tests can work on it.
+
+
+THIS DOESNOT QUITE WORK
+    ListMorphism.compose has no way return a composed object that is also a ListMorphism
+
+
+LONG-TERM MUDDLE:
+    Between structural-typed/inclusive typed Category/Element/Morphism, and actually
+    instantiated class for Category
+
+TRY:
+    Using the structural one
+    !! So, ListElement should probably be considered a virtual superclass of list
 """
 from typing import Callable
 
@@ -11,6 +24,9 @@ from ...support.methods import classproperty, pedanticmethod
 from .category import Category
 from .element import Element
 from .morphism import Morphism
+
+
+ListCallable = Callable[[list], list]
 
 
 class ListElement(list, Element):
@@ -24,21 +40,29 @@ class ListElement(list, Element):
 def list_identity(_list):
     return _list
 
-class ListMorphism(Callable[list, list], Morphism):
+
+class ListMorphism(Morphism):
     @classproperty
     def identity(cls):
         return list_identity
 
     @pedanticmethod
     def call(cls, self, element):
-        return 
+        return self(element)
+
+    @pedanticmethod
+    def compose(cls, self, other):
+        def composed(value):
+            return other(self(value))
+        return composed
 
     def __call__(self, element):
         return self(element)
 
-class ListCategory(Category):
-    pass
 
+class ListCategory(Category):
+    Element = ListElement
+    Morphism = ListMorphism
 
 
 # =======================================
@@ -48,6 +72,6 @@ class ListCategory(Category):
 from .simple import SimpleCategory, SimpleElement, SimpleMorphism
 
 SimpleListElement = SimpleElement('SimpleListElement', list)
-SimpleListMorphism = SimpleMorphism('SimpleListMorphism', Callable[[list], list])
+SimpleListMorphism = SimpleMorphism('SimpleListMorphism', ListCallable)
 SimpleListCategory = SimpleCategory('SimpleListCategory', SimpleListElement, SimpleListMorphism)
-SimpleListCategory2 = SimpleCategory('SimpleListCategory2', list, Callable[[list], list])
+SimpleListCategory2 = SimpleCategory('SimpleListCategory2', list, ListCallable)
