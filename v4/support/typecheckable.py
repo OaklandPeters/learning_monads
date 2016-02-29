d53de6ac5a96d139b7d8f495c712fd38a7f7114c
Oakland Peters <oakland.peters@gmail.com>
2016-02-29 13:15:26 -0500

    Bugfix: typecheckablemeta with TypeCheckable

--- v4/support/typecheckable.py
+++ v4/support/typecheckable.py
@@ -1,20 +1,30 @@
 from abc import ABCMeta, abstractclassmethod
 
 
+def _has_concrete_method(cls, name):
+    if any(name in klass.__dict__ for klass in cls.__mro__):
+        method = getattr(cls, name)
+        if not getattr(method, '__isabstractmethod__', False):
+            return True
+    return False
+
+
 class TypeCheckableMeta(ABCMeta):
     """Makes isinstance and issubclass overrideable."""
 
     def __instancecheck__(cls, instance):
-        if any('__instancecheck__' in klass.__dict__ for klass in cls.__mro__):
+        if _has_concrete_method(cls, '__instancecheck__'):
             return cls.__instancecheck__(instance)
         else:
-            return type.__instancecheck__(cls, instance)
+            # If __instancheck__ NOT redefined
+            #   OR if it is redefined, but as an abstractmethod
+            return ABCMeta.__instancecheck__(cls, instance)
 
     def __subclasscheck__(cls, subclass):
-        if any('__subclasscheck__' in klass.__dict__ for klass in cls.__mro__):
+        if _has_concrete_method(cls, '__subclasscheck__'):
             return cls.__subclasscheck__(subclass)
         else:
-            return type.__subclasscheck__(cls, subclass)
+            return ABCMeta.__subclasscheck__(cls, subclass)
 
 
 class TypeCheckable(metaclass=TypeCheckableMeta):
@@ -25,6 +35,33 @@ class TypeCheckable(metaclass=TypeCheckableMeta):
     @abstractclassmethod
     def __subclasscheck__(cls, subclass):
         return NotImplemented
+
+
+def meets(klass: 'type', abstracts: 'Union[Sequence[str]]'):
+    return not missing_abstracts(klass, abstracts)
+
+
+def _missing_abstracts(klass, abstracts):
+    for method in abstracts:
+        for base in klass.__mro__:
+            if method in base.__dict__:
+                break
+        else:
+            yield method
+
+
+def missing_abstracts(klass, abstracts):
+    if hasattr(abstracts, '__abstractmethods__'):
+        abstracts = abstracts.__abstractmethods__
+    return list(_missing_abstracts(klass, abstracts))
+
+
+def meets_interface(klass, interface):
+    """Maybe problem - pulling __abstractmethods__ from interface might
+    be a problem. collections.abc does not do this - and I'm assuming there is a
+    good reason.
+    """
+    return meets(klass, interface.__abstractmethods__)
 
 
 def type_check(value, *klasses, name='object'):
