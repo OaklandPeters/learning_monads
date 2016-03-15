56290f44e7f407f3f2ac58f9732ee9c47fad1078
Oakland Peters <oakland.peters@gmail.com>
2016-03-15 11:10:31 -0400

    Reorganizing abstract hierarchy: basis ABCs

--- v4/foundations/category.py
+++
@@ -1,6 +0,0 @@
-"""
-Difference in terms:
-----------------------
-In category-theory, what is called the *objects* in a category, we are calling the *elements* of a category - to prevent name-collusions (and confusions) with Pythons builtin 'object' class.
-Note - Python's 'object' class, actually forms the 'elements' for the category of all objects representable in Python (which I'm calling Pysk).
-"""
