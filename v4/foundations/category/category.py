68860957e3173676c5c7349bda63a84fb1c8254f
Oakland Peters <oakland.peters@gmail.com>
2016-03-20 11:19:17 -0400

    Reorganization

--- v4/foundations/category/category.py
+++ v4/foundations/category/category.py
@@ -1,3 +1,10 @@
+"""
+Difference in terms:
+----------------------
+In category-theory, what is called the *objects* in a category, we are calling the *elements* of a category - to prevent name-collusions (and confusions) with Pythons builtin 'object' class.
+Note - Python's 'object' class, actually forms the 'elements' for the category of all objects representable in Python (which I'm calling Pysk).
+
+"""
 from ...support.typecheckable import TypeCheckableMeta, meets
 from ...support.methods import abstractclassproperty
 
@@ -22,3 +29,7 @@ class Category(metaclass=TypeCheckableMeta):
         if cls is Category:
             return meets(C, cls.__abstractmethods__)
         return NotImplemented
+
+
+def is_in_category(obj, category):
+    return isinstance(obj, category.Element) or isinstance(obj, category.Morphism)
