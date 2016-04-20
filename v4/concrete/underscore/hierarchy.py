064ec931c74c81753ef8fbc37395987df8007ac6
Oakland Peters <oakland.peters@gmail.com>
2016-04-20 17:54:22 -0400

    Fill in more Underscore structure stubs

--- v4/concrete/underscore/hierarchy.py
+++ v4/concrete/underscore/hierarchy.py
@@ -1,17 +1,66 @@
 #!/usr/bin/env python3
 """
+This bastard needs PyskSpace.
+Or better yet, ArgumentsSpace
+
+
+Todo:
+* Put a version of psyk into concrete/pysk/
+* Add cofunctor to functor.py
+* Draft category.CategorySugar, to resemble my work for Pipe
+* Draft ABC version of Arrow
 """
-from ...foundations import space, functor
+from ...foundations import space, functor, monad, arrow
 from ...groups import category
+from .. import pysk
 
 
-class USpace(space.Space):
+class UMorphism(space.Morphism, space.MorphismSugar):
     pass
 
-class UMorphism(space.Morphism):
-    pass
 
 class UElement(space.Element):
     pass
 
 
+class USpace(space.Space):
+    Element = UElement
+    Morphism = UMorphism
+
+
+class UCategory(USpace, category.Category, category.CategorySugar):
+    pass
+
+
+class UArrow(UCategory, arrow.Arrow):
+    pass
+
+
+class UFunctor(functor.Functor):
+    Domain = UCategory
+    Codomain = pysk.PyskCategory
+
+
+class UCofunctor(functor.Cofunctor):
+    Domain = pysk.PyskCategory
+    Codomain = UCategory
+
+
+class UFunctorCategory(UFunctor, UCategory):
+    pass
+
+
+class UCofunctorCategory(UCofunctor, UCategory):
+    pass
+
+
+class UMonad(UFunctorCategory, monad.Monad):
+    pass
+
+
+class UComonad(UCofunctorCategory, monad.Monad):
+    pass
+
+
+class UContext(UMonad, UComonad, UArrow):
+    pass
