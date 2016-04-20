86f92a0840c3d7bde800ba1cba5fd675c313fc48
Oakland Peters <oakland.peters@gmail.com>
2016-04-20 15:03:07 -0400

    Stubs of concreate versions.

---
+++ v4/concrete/underscore/hierarchy.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+"""
+from ...foundations import space, functor
+from ...groups import category
+
+
+class USpace(space.Space):
+    pass
+
+class UMorphism(space.Morphism):
+    pass
+
+class UElement(space.Element):
+    pass
+
+
---
+++ v4/tests/test_underscore.py
@@ -0,0 +1,13 @@
+"""
+python3 -u unittest v4.tests.test_underscore
+"""
+
+from v4.concrete.underscore import hierarchy
+
+
+print()
+print("hierarchy:", type(hierarchy), hierarchy)
+print()
+import ipdb
+ipdb.set_trace()
+print()
